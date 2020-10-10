7eea3a22918205f69b2ea7e8dc4d44fdcb977463
Cecil <cecil.cao@mail.utoronto.ca>
2020-10-10 18:23:09 -0400

    Edit EnemyMovement behaviour
    Changed the enemy's movement to MovePosition instead of transform.
    Change var name rigidBody -> rb

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,16 +6,25 @@ public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float rayLength;
+    public Rigidbody rb;
 
+    private void Start() {
+        rb = this.GetComponent<Rigidbody>();
+    }
     void FixedUpdate() {
-        transform.Translate(0,0, moveSpeed * Time.deltaTime);
         detectLedge();
+
+        // NOTE: It can potentially make the character pass through other objects if the speed
+        //       is set too fast!
+        rb.MovePosition(transform.position + (Vector3.forward * moveSpeed * Time.deltaTime));
+        
     }
 
     void detectLedge() {
-        if (!Physics.Raycast(transform.position, Vector3.down, rayLength))
-        {
-            // Debug.Log("Reached Ledge");
+        //TODO: Make it so that it doesn't check the ledge when falling.
+        //TODO: Fix the bug where it reaches the edge by a player pushing it and can't move.
+        if (!Physics.Raycast(transform.position, Vector3.down, rayLength)) {
+            Debug.Log("Reached Ledge");
             moveSpeed = -moveSpeed;
         }
     }
