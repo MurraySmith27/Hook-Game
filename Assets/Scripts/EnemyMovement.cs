5ef367465c981b65b0c1b7da00edb760a701b6ae
Cecil <cecil.cao@mail.utoronto.ca>
2020-10-10 00:10:09 -0400

    Add Enemy
    Added an enemy and gave it collision and movement.
    Checks if it reaches the ledge and turns it around.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rayLength;
+
+    void FixedUpdate() {
+        transform.Translate(0,0, moveSpeed * Time.deltaTime);
+        detectLedge();
+    }
+
+    void detectLedge() {
+        if (!Physics.Raycast(transform.position, Vector3.down, rayLength))
+        {
+            // Debug.Log("Reached Ledge");
+            moveSpeed = -moveSpeed;
+        }
+    }
+}
