cc2a9c780f789aa3998ada57191059f5860bc4a1
Murray Smith <murraysmith2727@gmail.com>
2020-10-18 15:13:32 -0400

    Merged in Tony's changes to the movement system

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    const float MidAirMultiplier = 0.5f;
+    const float SpeedLimit = 15f;
+    const float InitialSpeed = 8f;
+    const float JumpBufferTime = 0.1f;
+    const float JumpDistance = 4f;
+
+    // each second the player speeds up by 0.25 m/s
+    const float Accel = 2.5f;
+    
+    public int zDirection;
+    public bool needToJump;
+
+    // jump buffer
+    public float jumpBuffer;
+    public bool isGrounded = true;
+
+    CapsuleCollider col;
+    Rigidbody rig;
+
+    // used to check if the player is grappling to something
+    GrapplingMovement script;
+    MovementController controller;
+
+    void Awake()
+    {
+        col = GetComponent<CapsuleCollider>();
+        rig = GetComponent<Rigidbody>();
+        script = GetComponent<GrapplingMovement>();
+        controller = GetComponent<MovementController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            needToJump = true;
+        }
+
+        // this stuff handles z axis movement        
+        zDirection = Input.GetKeyDown(KeyCode.A) ? -1 : (Input.GetKeyDown(KeyCode.D) ? 1 : zDirection);
+
+        if (Input.GetKeyUp(KeyCode.A) && zDirection == -1)
+        {
+            zDirection = Input.GetKey(KeyCode.D) ? 1 : 0;
+        }
+        else if (Input.GetKeyUp(KeyCode.D) && zDirection == 1)
+        {
+            zDirection = Input.GetKey(KeyCode.A) ? -1 : 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        HandleKeyboardMouseInputs();
+    }
+
+    void HandleKeyboardMouseInputs()
+    {
+        // always reset buffer to JumpBufferTime if not grounded
+        if (!controller.isGrounded)
+        {
+            jumpBuffer = JumpBufferTime;
+        }
+
+        if (needToJump && jumpBuffer <= 0)
+        {
+            rig.AddForce(new Vector3(0, (float)Math.Sqrt(Physics.gravity.y * -2 * JumpDistance), 0), ForceMode.VelocityChange);
+        }
+
+        needToJump = false;
+        jumpBuffer -= Time.fixedDeltaTime;
+
+        if (zDirection == 0 && controller.isGrounded)
+        {
+            rig.velocity = new Vector3(rig.velocity.x, rig.velocity.y, 0);
+        }
+        else
+        {        
+            if (rig.velocity.z == 0 || rig.velocity.z * zDirection < 0)
+            {
+                rig.velocity = new Vector3(rig.velocity.x, rig.velocity.y, 0);
+                rig.AddForce(new Vector3(0, 0, zDirection * InitialSpeed * (isGrounded ? 1 : 0.5f)), ForceMode.VelocityChange);
+            }
+            var vel = new Vector3(0, 0, zDirection * Accel * (isGrounded ? 1 : 0.5f));
+            rig.AddForce(vel, ForceMode.Acceleration);
+        }
+
+    }
+}
---
+++ Assets/Scripts/GrapplingMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingMovement : MonoBehaviour
+{
+    const float StopThreshold = 0.6f;
+
+    Rigidbody rig;
+    SpringJoint joint;
+
+
+    GameObject grappleTo;
+    GameObject grappleFrom;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && grappleTo == null)
+        {
+            grappleTo = GetGrapplableUnderCursor();
+
+            if (grappleTo)
+                rig.velocity = Vector3.zero;
+        }
+        else if (Input.GetMouseButtonDown(1) && grappleFrom == null)
+        {
+            grappleFrom = GetGrapplableUnderCursor();
+        }
+
+        ProcessGrappleTo();
+        ProcessGrappleFrom();
+
+    }
+
+    GameObject GetGrapplableUnderCursor()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 8))
+        {
+            return hit.transform.gameObject;
+        }
+
+        return null;
+    }
+
+    void ProcessGrappleTo()
+    {
+        if (grappleTo == null)
+            return;
+
+        rig.useGravity = false;
+
+        var src = transform.position;
+        var dst = grappleTo.transform.position;
+
+        float distance = Vector3.Distance(src, dst);
+
+        if (distance < StopThreshold)
+        {
+            grappleTo = null;
+            rig.useGravity = true;
+        }
+
+        rig.AddForce(dst - src, ForceMode.Acceleration); 
+    }
+
+    void ProcessGrappleFrom()
+    {
+        if (grappleFrom == null)
+            return;
+        
+        var src = grappleFrom.transform.position;
+        var dst = transform.position;
+
+        float distance = Vector3.Distance(src, dst);
+
+        if (distance < StopThreshold)
+        {
+            grappleFrom = null;
+        }
+
+        grappleFrom.GetComponent<Rigidbody>().AddForce(dst - src, ForceMode.Acceleration);
+    }
+
+    public bool IsGrapplingTo()
+    {
+        return grappleTo != null;
+    }
+
+}
