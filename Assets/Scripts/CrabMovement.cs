51d5b57c4f3db8285906b8400e3ce506e58e93fb
TheXanMan321 <73006103+TheXanMan321@users.noreply.github.com>
2020-11-10 14:21:13 -0500

    crab enemy
    Different things for Crab Enemy.

--- Assets/Scripts/CrabMovement.cs
+++ Assets/Scripts/CrabMovement.cs
@@ -2,35 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crab_movement : EnemyMovement
+public class CrabMovement : EnemyMovement
 {
-
     public Animator anim;
-    
+
     // Start is called before the first frame update
     public float anim_speed = 1;
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         anim.speed = anim_speed;
-        anim.SetInteger("Direction", (int) (moveSpeed / Mathf.Abs(moveSpeed) + 0.1));
+        anim.SetInteger("Direction", (int)(moveSpeed / Mathf.Abs(moveSpeed) + 0.1));
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         detectLedge();
 
         // NOTE: It can potentially make the character pass through other objects if the speed
         //       is set too fast!
         rb.MovePosition(transform.position + (Vector3.forward * moveSpeed * Time.deltaTime));
-        
+
     }
 
-    void detectLedge() {
+    void detectLedge()
+    {
         //TODO: Make it so that it doesn't check the ledge when falling.
         //TODO: Fix the bug where it reaches the edge by a player pushing it and can't move.
-        if (!Physics.Raycast(transform.position, Vector3.down, rayLength)) 
+        if (!Physics.Raycast(transform.position, Vector3.down, rayLength))
         {
             moveSpeed = -moveSpeed;
             anim.SetInteger("Direction", anim.GetInteger("Direction") * -1);
         }
     }
 }
+
